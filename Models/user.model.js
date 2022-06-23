fd337046ab82a9271720b3fe06457209aac686fe
xennia316 <xennia316@gmail.com>
2022-06-23 16:06:15 +0100

    user model adjusted

--- Models/user.model.js
+++ Models/user.model.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   name: String,
   email: String,
-  password: String,
+  password: Number,
 });
 
 const User = mongoose.model("User", UserSchema);
