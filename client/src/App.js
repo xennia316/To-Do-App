be3426bfab54c8b170a56915849504f9476a19ad
xennia316 <xennia316@gmail.com>
2022-06-24 15:29:25 +0100

    navbar test

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Nav from "./Nav/Nav";
 import ToDo from "./ToDo/ToDo";
 
 function App() {
   return (
     <div className="App">
-      <ToDo />
+      {/* <ToDo /> */}
+      <Nav />
     </div>
   );
 }
