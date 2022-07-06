d77e6c1ed45354325140c220642ef6bd8e826d8f
xennia316 <xennia316@gmail.com>
2022-07-06 23:08:08 +0100

    display full screen

---
+++ client-main/src/Display/Display.jsx
@@ -0,0 +1,62 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAdd } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+import ToDo from "../ToDo/ToDo";
+import styles from "./display.module.css";
+import ToDo2 from "../ToDo/ToDo2";
+
+const Display = () => {
+  const listOfList = [
+    { text: "Call granny" },
+    { text: "Rubbbish all around me " },
+    { text: "Tsuiiiiiiiiiiiiiiiiiiiiiiiiiiiiiip" },
+    { text: "Wow Sonia #laughing" },
+    { text: "Yam head like you" },
+    { text: "I'll report to Nadege :)" },
+    { text: "Sharap ya mouth yam #all-laughing" },
+  ];
+
+  const list2 = [
+    { text: "Call granny" },
+    { text: "Rubbbish all around me " },
+    { text: "Tsuiiiiiiiiiiiiiiiiiiiiiiiiiiiiiip" },
+  ];
+
+  const individualList = listOfList.map((elem, index) => {
+    return (
+      <>
+        <span className={styles.date}>date</span>
+        <ToDo key={index} text={elem.text} />
+      </>
+    );
+  });
+
+  const finished = list2.map((elem, index) => {
+    return (
+      <>
+        <span className={styles.date}>date</span>
+        <ToDo2 key={index} text={elem.text} />
+      </>
+    );
+  });
+  return (
+    <section className={styles.body}>
+      <section className={styles.top}>
+        <h2>To Do List</h2>
+      </section>
+      <section className={styles.topNext}>
+        <h2 className={styles.smaller}>To Do List</h2>
+        <button className={styles.btnTop}>
+          Add a task
+          <FontAwesomeIcon icon={faAdd} />
+        </button>
+      </section>
+      <section className={styles.todosection}>
+        {finished}
+        {individualList}
+      </section>
+    </section>
+  );
+};
+
+export default Display;
