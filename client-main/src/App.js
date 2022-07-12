6b73a1fc18bee431817477eba10bd0152eb7c722
xennia316 <xennia316@gmail.com>
2022-07-12 22:16:57 +0100

    launched modal

--- client-main/src/App.js
+++ client-main/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Display from "./Display/Display";
+import Modal from "./Modal/Modal";
 
 const App = () => {
   return (
     <section>
       <Display />
+      {/* <Modal /> */}
     </section>
   );
 };
