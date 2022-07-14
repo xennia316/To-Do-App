6d64c8247f28c8484c5a223c3cdf4e6395a5f7c6
xennia316 <xennia316@gmail.com>
2022-07-14 21:07:41 +0100

    implemented post route to backend

--- client-main/src/Modal/Modal.jsx
+++ client-main/src/Modal/Modal.jsx
@@ -1,15 +1,56 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import styles from "./Modal.module.css";
-import axios from "axios"
+import axios from "axios";
 
 const Modal = (props) => {
+  const today = new Date();
+  const day = today.toDateString();
+  // console.log(day);
+  const clear = () => {
+    setTask({ content: "", dueDate: day, priority: 1 });
+  };
 
-  const [task, setTask] = useState("");
-  const [priority, setPriority] = useState(1);
+  const [task, setTask] = useState({
+    content: "",
+    dueDate: "",
+    priority: 1,
+  });
 
-  const handleTaskChange = () =>{
-    
-  }
+  const handleTaskChange = (e) => {
+    const { name, value } = e.target;
+    setTask({
+      ...task,
+      [name]: value,
+    });
+  };
+
+  const checkProperties = (task) => {
+    if (
+      task.content === "" ||
+      task.priority < 1 ||
+      task.priority > 3 ||
+      task.dueDate < today
+    ) {
+      alert("This information cannot be registered");
+      setTask({ content: "", dueDate: "", priority: 1 });
+      console.log(task);
+      return false;
+    }
+    return true;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (checkProperties(task)) {
+      let result = await axios.post("/api/todo/add", task).then((data) => data);
+      if (result.status === 200) {
+        localStorage.setItem("todo", JSON.stringify(result.data.data));
+        clear();
+      } else {
+        alert("Unsuccessful!");
+      }
+    }
+  };
 
   if (!props.show) {
     return null;
@@ -28,22 +69,42 @@ const Modal = (props) => {
               {" "}
               Task
               <input
+                name="content"
+                value={task.content}
                 className={styles.input}
                 type="text"
                 placeholder="Please enter your next task here"
+                onChange={handleTaskChange}
+              />
+            </label>{" "}
+            <label>
+              {" "}
+              Due Date
+              <input
+                value={task.dueDate}
+                className={styles.input}
+                type="date"
+                name="dueDate"
+                placeholder="PExpiry date for task here"
+                onChange={handleTaskChange}
               />
             </label>{" "}
             <label>
               priority
               <input
+                value={task.priority}
+                name="priority"
                 className={styles.input}
                 type="Number"
                 max="3"
                 min="1"
                 placeholder="Enter a priority"
+                onChange={handleTaskChange}
               />
             </label>
-            <button className={styles.btn}>Submit</button>
+            <button className={styles.btn} onClick={handleSubmit}>
+              Submit
+            </button>
           </form>
         </section>
       </section>
