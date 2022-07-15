740be0f745cc8c85a6edfec5a36e6ca2b9e2c7d6
xennia316 <xennia316@gmail.com>
2022-07-15 14:27:17 +0100

    milestone check

--- client-main/src/ToDo/ToDo.jsx
+++ client-main/src/ToDo/ToDo.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./toDo.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -6,8 +6,15 @@ import {
   faTrash,
   faPen,
 } from "@fortawesome/free-solid-svg-icons";
+import Modal from "../Modal/Modal";
 
 const ToDo = (param) => {
+  const [editor, setEditor] = useState(null);
+
+  const handleClick = () => {
+    return <Modal />;
+  };
+
   return (
     <section className={styles.body}>
       <section className={styles.twoSide}>
@@ -17,7 +24,7 @@ const ToDo = (param) => {
         <section className={styles.content}>{param.text}</section>
       </section>
       <section className={styles.twoSide}>
-        <section className={styles.edit}>
+        <section className={styles.edit} onClick={handleClick}>
           <FontAwesomeIcon icon={faPen} className={styles.icon} />
         </section>
         <section className={styles.trash}>
